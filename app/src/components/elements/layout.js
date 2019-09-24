819cde24006723fb553435788120af0079fd2617
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-09-24 22:31:15 +0800

    add grommet

--- app/src/components/elements/layout.js
+++ app/src/components/elements/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Box } from "rebass"
 import { useQuery } from "@apollo/react-hooks"
+import { Grommet } from "grommet"
 
 import { Header, Footer } from "../elements"
 import { CURRENT_USER_QUERY } from "../apollo/graphql"
@@ -11,20 +12,22 @@ export default function Layout({ children, location }) {
   const { loading, error, data } = useQuery(CURRENT_USER_QUERY)
 
   return (
-    <Box
-      style={{
-        margin: "0 auto",
-        maxWidth: `var(--max-width)`,
-        // for ios position sticky
-        display: `block`,
-      }}
-    >
-      <Header location={location} />
-      <Box as="main" style={{ minHeight: `100vh` }}>
-        {children}
+    <Grommet>
+      <Box
+        style={{
+          margin: "0 auto",
+          maxWidth: `var(--max-width)`,
+          // for ios position sticky
+          display: `block`,
+        }}
+      >
+        <Header location={location} />
+        <Box as="main" style={{ minHeight: `100vh` }}>
+          {children}
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </Grommet>
   )
 }
 
