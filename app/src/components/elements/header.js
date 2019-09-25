056c33e0c300e3fae11c70673b559baed38cd01c
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-09-25 08:19:00 +0800

    navbar position fixed

--- app/src/components/elements/header.js
+++ app/src/components/elements/header.js
@@ -14,8 +14,10 @@ export default function Header({ location }) {
       p={[2]}
       opacity=".92"
       style={{
-        position: `sticky`,
+        position: `fixed`,
         top: `0`,
+        left: `0`,
+        right: `0`,
         zIndex: `1`,
         borderBottom: `1px solid var(--light-2)`,
       }}
