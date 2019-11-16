1bc7754637945a3b989f31176cefffca35d5cda0
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-11-16 10:52:10 +0700

    listen for process.env.PORT
    Because heroku deploy assigns the app a port

--- server/index.js
+++ server/index.js
@@ -19,6 +19,6 @@ const server = new ApolloServer({
   },
 })
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 4000).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
 })
