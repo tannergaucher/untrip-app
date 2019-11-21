db5962973c8f9329651ab0e8323e61dd14917a1d
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-11-21 19:21:06 +0700

    Update menu.js

--- client/src/components/elements/menu.js
+++ client/src/components/elements/menu.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import styled from "styled-components"
 import { useQuery } from "@apollo/react-hooks"
-import { List } from "grommet-icons"
 
 import { Layer } from "grommet"
 import { Menu as MenuIcon } from "grommet-icons"
