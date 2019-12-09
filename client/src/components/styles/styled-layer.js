5d9c3628dbb2eb2f072292d715c9114e0a09f5d2
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-12-09 13:45:18 +0800

    Update styled-layer.js

--- client/src/components/styles/styled-layer.js
+++ client/src/components/styles/styled-layer.js
@@ -8,6 +8,7 @@ const StyledLayer = styled(Layer)`
   color: var(--text-color);
   border-radius: var(--radius);
   background: var(--bg-1);
+  border: 2px solid var(--text-color);
 
   @media (max-width: 1024px) {
     width: 100vw;
